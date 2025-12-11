1bd99197e74dd81b814d41b05e324c162b2db9ea
rjnaisu <naismizzle@gmail.com>
2025-12-11 22:30:41 +1100

    refactored for 4.2

---
+++ part4/blogs/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const config = require("./utils/config");
+const logger = require("./utils/logger");
+const mongoose = require("mongoose");
+const middleware = require("./utils/middleware");
+const blogRouter = require("./controllers/blogs");
+
+const app = express();
+app.use(express.json());
+
+logger.info("Starting server", config.MONGODB_URI);
+
+mongoose
+  .connect(config.MONGODB_URI, { family: 4 })
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((err) => {
+    console.log(`Error connecting to DB ${err}`);
+  });
+
+app.use(express.static("dist"));
+app.use(middleware.requestLogger);
+
+app.use("/api/blogs", blogRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
